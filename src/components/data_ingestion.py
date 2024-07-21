f85111241edf85c5be72e3f1b762204915bc51cb
Surya <suryasaith@gmail.com>
2024-07-21 01:19:23 -0500

    Data Preparation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,11 +4,11 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 from dataclasses import dataclass
+from src.components.data_preparation import DataPreparation
+from src.components.data_preparation import DataPreparationConfig
 
 @dataclass
 class DataIngestionConfig:
-    clientgender_data_path: str = os.path.join("artifacts", "clientgender.csv")
-    purchase_history_data_path: str = os.path.join("artifacts", "purchase_history.csv")
     clientgender_rawdata_path: str = os.path.join("artifacts", "clientgender_rawdata.csv")
     purchase_history_rawdata_path: str = os.path.join("artifacts", "purchase_history_rawdata.csv")
 
@@ -22,22 +22,22 @@ class DataIngestion:
             df1=pd.read_csv('notebook\data\ClientGender.csv')
             logging.info('Read the clientgender dataset as dataframe')
 
-            os.makedirs(os.path.dirname(self.ingestion_config.clientgender_data_path),exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.clientgender_rawdata_path),exist_ok=True)
 
             df1.to_csv(self.ingestion_config.clientgender_rawdata_path,index=False,header=True)
 
             df2=pd.read_csv('notebook\data\PurchaseHistory.csv')
             logging.info('Read the purchase history dataset as dataframe')
 
-            os.makedirs(os.path.dirname(self.ingestion_config.purchase_history_data_path),exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.purchase_history_rawdata_path),exist_ok=True)
 
             df2.to_csv(self.ingestion_config.purchase_history_rawdata_path,index=False,header=True)
 
             logging.info("Ingestion of both the datas is completed")
 
             return(
-                self.ingestion_config.clientgender_data_path,
-                self.ingestion_config.purchase_history_data_path
+                self.ingestion_config.clientgender_rawdata_path,
+                self.ingestion_config.purchase_history_rawdata_path
 
             )
         except Exception as e:
@@ -45,7 +45,10 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    clientgender_data, purchase_history_data = obj.initiate_data_ingestion()
+
+    data_prep = DataPreparation()
+    data_prep.merge_data_objects(clientgender_data, purchase_history_data)
 
 
 
---
+++ src/components/data_preparation.py
@@ -0,0 +1,70 @@
+import sys
+from dataclasses import dataclass
+
+import numpy as np 
+import pandas as pd
+
+from src.exception import CustomException
+from src.logger import logging
+import os
+
+
+
+@dataclass
+class DataPreparationConfig:
+    merged_data_path: str=os.path.join('artifacts',"merged_data.csv")
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    
+class DataPreparation:
+    def __init__(self):
+        self.data_preparation_config=DataPreparationConfig()
+        
+    def merge_data_objects(self, clientgender_rawdata_path, purchase_history_rawdata_path):
+        '''
+        This function is responsible for converting/correcting data types and then merging two datasets
+        
+        '''
+        try:
+
+            df1 = pd.read_csv(clientgender_rawdata_path)
+            df2 = pd.read_csv(purchase_history_rawdata_path)
+
+            logging.info("Read the datasets for preparation")
+
+            df1['client_id'] = pd.to_numeric(df1['client_id'], errors='coerce')
+            df1 = df1.dropna(subset=['client_id'])
+            df1.reset_index(drop=True, inplace=True)
+
+            logging.info("Converted client_id to numeric in clientgender dataset")
+
+            df2['client_id'] = pd.to_numeric(df2['client_id'], errors='coerce')
+            df2 = df2.dropna(subset=['client_id'])
+            df2.reset_index(drop=True, inplace=True)
+
+            logging.info("Converted client_id to numeric in purchase_history dataset")
+
+            merged_df = pd.merge(df1, df2, on='client_id', how='inner')
+
+            logging.info("Merged two cleaned datasets")
+
+            merged_df['purchase_amount'] = pd.to_numeric(merged_df['purchase_amount'], errors='coerce')
+
+            merged_df['date'] = pd.to_datetime(merged_df['date'], errors='coerce')
+
+            merged_df = merged_df.dropna(subset=['date', 'client_id', 'purchase_amount'])
+
+            logging.info("Cleaned date, purchase amount and dropped null values in Merged dataset")
+
+            merged_df.to_csv(self.data_preparation_config.merged_data_path,index=False,header=True)
+
+            logging.info("Saved merged dataset to artifacts")
+
+            return(
+                self.data_preparation_config.merged_data_path
+
+            )
+  
+        except Exception as e:
+            raise CustomException(e,sys)
+        
